5b06797ed70def2f04832b7652deff58612c6806
Miguel FAcchini <59362540+miguelfacchini@users.noreply.github.com>
2019-12-30 09:10:37 -0300

    Create manutencao.sh

---
+++ manutencao.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+LOGFILE=/suporte/manutencao.log
+/usr/local/pgsql/bin/./vacuumdb -U postgres -a -z
+if [ $? = 0 ]; then
+    echo "`date +%c` - Vacuum realizado com Sucesso" >> $LOGFILE
+else
+    echo "`date +%c` - ERRO ao realizar vacuum." >> $LOGFILE
+fi
